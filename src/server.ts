3c8f323412146b0ac4c6e9b4ef0d685d2be64bf3
estuhub <107235863+estuhub@users.noreply.github.com>
2024-01-16 19:42:22 +0700

    create server

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next-utils";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+const start = async () => {
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+      },
+    },
+  });
+
+  // Selfhosting
+  app.use((req, res) => nextHandler(req, res));
+
+  nextApp.prepare().then(() => {
+    app.listen(PORT, async () => {});
+  });
+};
+
+start();
