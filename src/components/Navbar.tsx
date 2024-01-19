3b88a37ff0f5fdb8427fe70e250e2b0dc06bd73d
estuhub <107235863+estuhub@users.noreply.github.com>
2024-01-19 17:50:46 +0700

    create UserAccountNav component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { buttonVariants } from "./ui/button";
 import Cart from "./Cart";
 import { getServerSideUser } from "@/lib/payload.utils";
 import { cookies } from "next/headers";
+import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async () => {
   const nextCookies = cookies();
@@ -42,7 +43,9 @@ const Navbar = async () => {
                     <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
                   )}
 
-                  {user ? null : (
+                  {user ? (
+                    <UserAccountNav user={user} />
+                  ) : (
                     <Link
                       href="/sign-up"
                       className={buttonVariants({ variant: "ghost" })}
