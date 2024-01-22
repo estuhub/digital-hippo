f1f91b941decf6f5dd0f10ad5fb1bada26e8297f
estuhub <107235863+estuhub@users.noreply.github.com>
2024-01-22 18:39:47 +0700

    create Stripe webhook

---
+++ src/webhooks.ts
@@ -0,0 +1,114 @@
+import express from "express";
+import { WebhookRequest } from "./server";
+import { stripe } from "./lib/stripe";
+import type Stripe from "stripe";
+import { getPayloadClient } from "./get-payload";
+import { Product } from "./payload-types";
+import { Resend } from "resend";
+import { ReceiptEmailHtml } from "./components/emails/ReceiptEmail";
+
+// Initialize Resend with the provided API key
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+// Handler for the Stripe webhook
+export const stripeWebhookHandler = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  // Validate that the request comes from Stripe
+  // Update the _isPaid value of this order that we passed as metadata
+  // Send receipt email
+  const webhookRequest = req as any as WebhookRequest;
+  const body = webhookRequest.rawBody;
+  const signature = req.headers["stripe-signature"] || "";
+
+  let event;
+  try {
+    // Verify the Stripe webhook signature
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET || ""
+    );
+  } catch (err) {
+    // Handle verification failure
+    return res
+      .status(400)
+      .send(
+        `Webhook Error: ${err instanceof Error ? err.message : "Unknown Error"}`
+      );
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  if (!session?.metadata?.userId || !session?.metadata?.orderId) {
+    // Check if user and order information is present in metadata
+    return res.status(400).send(`Webhook Error: No user present in metadata`);
+  }
+
+  if (event.type === "checkout.session.completed") {
+    const payload = await getPayloadClient();
+
+    // Fetch user information
+    const { docs: users } = await payload.find({
+      collection: "users",
+      where: {
+        id: {
+          equals: session.metadata.userId,
+        },
+      },
+    });
+
+    const [user] = users;
+
+    if (!user) return res.status(404).json({ error: "No such user exists." });
+
+    // Fetch order information
+    const { docs: orders } = await payload.find({
+      collection: "orders",
+      depth: 2,
+      where: {
+        id: {
+          equals: session.metadata.orderId,
+        },
+      },
+    });
+
+    const [order] = orders;
+
+    if (!order) return res.status(404).json({ error: "No such order exists." });
+
+    // Update the order's _isPaid value to true
+    await payload.update({
+      collection: "orders",
+      data: {
+        _isPaid: true,
+      },
+      where: {
+        id: {
+          equals: session.metadata.orderId,
+        },
+      },
+    });
+
+    // Send receipt email using Resend
+    try {
+      const data = await resend.emails.send({
+        from: "DigitalHippo <hello@joshtriedcoding.com>",
+        to: [user.email],
+        subject: "Thanks for your order! This is your receipt.",
+        html: ReceiptEmailHtml({
+          date: new Date(),
+          email: user.email,
+          orderId: session.metadata.orderId,
+          products: order.products as Product[],
+        }),
+      });
+      res.status(200).json({ data });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  return res.status(200).send();
+};
