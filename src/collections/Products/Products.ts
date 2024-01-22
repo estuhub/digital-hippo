fefe927dcb88766c6ec4be8cc9bf3e003b1f2cc9
estuhub <107235863+estuhub@users.noreply.github.com>
2024-01-22 17:18:26 +0700

    add user and logic to create and update a product

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,5 +1,13 @@
+import { BeforeChangeHook } from "payload/dist/collections/config/types";
+import { Product } from "../../payload-types";
 import { PRODUCT_CATEGORIES } from "../../config";
 import { CollectionConfig } from "payload/types";
+import { stripe } from "../../lib/stripe";
+
+const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
+  const user = req.user;
+  return { ...data, user: user.id };
+};
 
 // Collection configuration for the "Products" collection
 export const Products: CollectionConfig = {
@@ -10,8 +18,58 @@ export const Products: CollectionConfig = {
   },
   // Access control settings for various operations (read, update, delete, create)
   access: {},
+  // Hooks configuration to execute functions before changes
+  hooks: {
+    beforeChange: [
+      addUser, // Execute addUser hook
+      async (args) => {
+        // Additional hook logic for create and update operations
+        if (args.operation === "create") {
+          // Logic for creating a new product
+          const data = args.data as Product;
+
+          // Create a new product on Stripe
+          const createdProduct = await stripe.products.create({
+            name: data.name,
+            default_price_data: {
+              currency: "USD",
+              unit_amount: Math.round(data.price * 100),
+            },
+          });
+
+          // Update the product data with Stripe IDs
+          const updated: Product = {
+            ...data,
+            stripeId: createdProduct.id,
+            priceId: createdProduct.default_price as string,
+          };
+
+          return updated;
+        } else if (args.operation === "update") {
+          // Logic for updating an existing product
+          const data = args.data as Product;
+
+          // Update the product on Stripe
+          const updatedProduct = await stripe.products.update(data.stripeId!, {
+            name: data.name,
+            default_price: data.priceId!,
+          });
+
+          // Update the product data with new Stripe IDs
+          const updated: Product = {
+            ...data,
+            stripeId: updatedProduct.id,
+            priceId: updatedProduct.default_price as string,
+          };
+
+          return updated;
+        }
+      },
+    ],
+  },
   // Fields configuration for the collection
   fields: [
+    // Relationship field for the 'user' associated with the product
     {
       name: "user",
       type: "relationship",
@@ -22,17 +80,20 @@ export const Products: CollectionConfig = {
         condition: () => false, // Hide "user" field in the admin interface
       },
     },
+    // Text field for the product name
     {
       name: "name",
       label: "Name",
       type: "text",
       required: true,
     },
+    // Textarea field for product details
     {
       name: "description",
       label: "Product Details",
       type: "textarea",
     },
+    // Number field for the product price
     {
       name: "price",
       label: "Price in USD",
@@ -41,6 +102,7 @@ export const Products: CollectionConfig = {
       min: 0,
       max: 1000,
     },
+    // Select field for the product category
     {
       name: "category",
       label: "Category",
@@ -48,6 +110,7 @@ export const Products: CollectionConfig = {
       required: true,
       options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
     },
+    // Relationship field for product files
     {
       name: "product_files",
       label: "Product File(s)",
@@ -56,6 +119,7 @@ export const Products: CollectionConfig = {
       required: true,
       hasMany: false,
     },
+    // Select field for product status
     {
       name: "approvedForSale",
       label: "Product Status",
@@ -81,6 +145,7 @@ export const Products: CollectionConfig = {
         },
       ],
     },
+    // Text field for the Stripe ID of the product (hidden in the admin interface)
     {
       name: "priceId",
       type: "text",
@@ -93,6 +158,7 @@ export const Products: CollectionConfig = {
         hidden: true, // Hide "priceId" field in the admin interface
       },
     },
+    // Text field for the Stripe ID of the product (hidden in the admin interface)
     {
       name: "stripeId",
       type: "text",
@@ -105,6 +171,7 @@ export const Products: CollectionConfig = {
         hidden: true, // Hide "stripeId" field in the admin interface
       },
     },
+    // Array field for product images
     {
       name: "images",
       label: "Product Images",
