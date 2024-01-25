633f0de96489669403b4e4576f7c2c43ccb5d27d
estuhub <107235863+estuhub@users.noreply.github.com>
2024-01-25 18:05:01 +0700

    create metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
+import { cn, constructMetadata } from "@/lib/utils";
+import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
+import "./globals.css";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "DigitalHippo",
-  description: "Your marketplace for high-quality digital assets.",
-};
+export const metadata = constructMetadata();
 
 export default function RootLayout({
   children,
